b67e9bd402aac767d4ae86fd502f8032249bc77d
Mohit Solanki <mohitsolanki7051@gmail.com>
2026-04-06 02:23:20 +0530

    Refactor server.js to improve structure and readability

--- server.js
+++ server.js
@@ -175,9 +175,6 @@ app.get('/t/:tunnelId', (req, res) => {
 </head>
 <body>
 <div class="banner">
-    <span class="dot" id="dot"></span>
-    <span>Tunnel &nbsp;|&nbsp; <span class="banner-status" id="status">Connecting...</span></span>
-
     <div class="banner-nav">
         <button class="nav-btn" id="btn-back" onclick="goBack()" disabled title="Back">
             <svg fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M15 19l-7-7 7-7"/></svg>
@@ -189,8 +186,11 @@ app.get('/t/:tunnelId', (req, res) => {
             <svg width="13" height="13" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/></svg>
             Home
         </button>
-        <span class="banner-id">ID: ${tunnelId}</span>
     </div>
+
+    <span class="dot" id="dot"></span>
+    <span>Tunnel &nbsp;|&nbsp; <span class="banner-status" id="status">Connecting...</span></span>
+    <span class="banner-id" style="margin-left:auto">ID: ${tunnelId}</span>
 </div>
 
 <div class="loading" id="loading">
